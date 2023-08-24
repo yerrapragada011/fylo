1e7139941aaf87e8dd24a9bbea937dbb906513a5
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-24 16:25:45 -0400

    JavaScript For Color Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,24 +10,24 @@ module.exports = {
         darkBlue3: 'hsl(219, 30%, 18%)',
         accentCyan: 'hsl(176, 68%, 64%)',
         accentBlue: 'hsl(198, 60%, 50%)',
-        lightRed: 'hsl(0, 100%, 63%)',
+        lightRed: 'hsl(0, 100%, 63%)'
       },
       fontFamily: {
         sans: ['Raleway', 'sans-serif'],
-        opensans: ['Open Sans', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif']
       },
       backgroundImage: (theme) => ({
         'logo-dark-mode': "url('../images/logo-dark-mode.svg')",
         'logo-light-mode': "url('../images/logo-light-mode.svg')",
         'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
-        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')",
-      }),
-    },
+        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')"
+      })
+    }
   },
   variants: {
     extend: {
-      backgroundImage: ['dark'],
-    },
+      backgroundImage: ['dark']
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
